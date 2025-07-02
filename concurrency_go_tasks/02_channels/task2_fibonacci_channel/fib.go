7fd20a8606e3d37419405ae46dfcbdb5242cf7a7
Daniil Lifatov <2004daniil9015@mail.ru>
2025-07-02 14:30:52 +0300

    channels task2 done

--- concurrency_go_tasks/02_channels/task2_fibonacci_channel/fib.go
+++ concurrency_go_tasks/02_channels/task2_fibonacci_channel/fib.go
@@ -3,7 +3,18 @@ package fibonacci
 // Fib возвращает канал, из которого можно читать первые n чисел Фибоначчи.
 func Fib(n int) <-chan int {
 	ch := make(chan int)
-	close(ch)
+	go func() {
+		defer close(ch)
+		a, b := 0, 1
+		if n > 0 {
+			ch <- a
+		}
+		for i := 1; i < n; i++ {
+			a, b = b, a+b
+			ch <- a
+		}
+	}()
+
 	// TODO: отправить последовательность Фибоначчи в канал
 	return ch
 }
